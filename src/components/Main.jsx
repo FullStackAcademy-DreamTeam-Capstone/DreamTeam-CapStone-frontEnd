2d0ca973878109275c92dcd7f12c80d9e92e8b89
minimyl <aandrew.myles@gmail.com>
2022-11-28 14:30:22 -0500

    starting routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Navbar } from "./"
+import { Navbar, Home, Register } from "./";
+import { createBrowserRouter, Routes, Route } from "react-router-dom";
 
 const Main = () => {
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
-  
+
   return (
     <div id="main">
       <div id="NavbarDiv">
-      <Navbar 
-      error = {error}
-      setError = {setError}
-      setloggedIn = {setLoggedIn}
-      />
+        <Navbar error={error} setError={setError} setloggedIn={setLoggedIn} />
       </div>
-      <div id="mainBody">
-        hello i am main
-      </div>
-  </div>
+      <Routes>
+        <Route
+          path="/register"
+          element={
+            <Register
+              error={error}
+              setError={setError}
+              setLoggedIn={setLoggedIn}
+            />
+          }
+        />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <div id="mainBody">hello i am main</div>
+    </div>
   );
 };
 
