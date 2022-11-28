e1e02b7599f09b3fa5d9cf212523246e95d171d6
timothy <timothybrennan8@gmail.com>
2022-11-28 14:58:51 -0500

    added basic products components and api adapter

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export {default as Main} from './Main'
 export {default as Navbar} from './Navbar'
 export {default as Register} from './Register'
 export {default as Home} from './Home'
+export {default as CreateProduct} from "./CreateProduct"
+export {default as Products} from "./Products"
