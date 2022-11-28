f3e12b06dfe45e76a9dc248e9688731d0b60b66f
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-11-28 11:46:49 -0600

    register handlesubmit function and forms

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Navbar } from "./"
 
 const Main = () => {
+  const [error, setError] = useState(null)
+  const [loggedIn, setLoggedIn] = useState(false);
+  
   return (
     <div id="main">
       <div id="NavbarDiv">
-      <Navbar/>
+      <Navbar 
+      error = {error}
+      setError = {setError}
+      setloggedIn = {setLoggedIn}
+      />
       </div>
       <div id="mainBody">
         hello i am main
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React from "react"
+import { useNavigate, NavLink } from "react-router-dom";
+import Register from "./Register"
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const error = props.error
+  const setError = props.setError
+  const setLoggedIn = props.setLoggedIn
+
+
   return (
     <div id="navbar">
-        <h2>I am navbar</h2>
-  </div>
+      <h2>I am navbar</h2>
+      <Register
+       error = {error}
+       setError = {setError} 
+       setLoggedIn = {setLoggedIn}
+       />
+    </div>
   );
 };
 
-export default Navbar;
+export default Navbar
