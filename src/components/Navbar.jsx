a0b90d75d23e5f92d3d8aff80cda24e1fc975eb0
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-11-28 13:57:45 -0600

    added popup to the register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,112 @@
-import React from "react"
+import React, {useState} from "react"
 import { useNavigate, NavLink } from "react-router-dom";
 import Register from "./Register"
+import Popup from "reactjs-popup";
 
 const Navbar = (props) => {
   const error = props.error
   const setError = props.setError
   const setLoggedIn = props.setLoggedIn
+
+
+  //REGISTER FUNCTION
+  const [registering, setRegistering] = useState(false)
+  const [registerInfo, setRegisterinfo] = useState({
+      username: "",
+      password: "",
+      name: "",
+      location: "",
+  })
+
+  async function handleSubmit(event) {
+      event.preventDefault()
+      const username = registerInfo.username
+      const password = registerInfo.password
+      const name = registerInfo.name
+      const location = registerInfo.location
+
+      if (password.length <= 6) {
+          setError ("Password must be more than 6 characters long.")
+      } else {
+          const response = await register(username, password, name, location)
+          localStorage.removeItem("token")
+
+          if  (response && response.token) {
+              localStorage.setItem("token", response.token)
+              setLoggedIn(response.token)
+              setRegistering(false)
+              setError(null)
+          } else {
+              setLoggedIn(false)
+              setError("User already exists")
+          }
+      }
+  }
+
   
-
-
   return (
     <div id="navbar">
       <h2>I am navbar</h2>
+
+      <Popup trigger={<button> Register </button>}>
+
+      <form onSubmit={handleSubmit}>
+
+            <h4> Register </h4>
+            <label htmlFor="username1"> Username: </label>
+            <input
+            id="username1"
+            type="text"
+            onChange={(e) => setRegisterinfo({...registerInfo, username: e.target.value})}
+            value = {registerInfo.username}
+            required
+            />
+
+            <br />
+
+            <label htmlFor="password2"> Password: </label>
+            <input
+            id="password2"
+            type="password"
+            onChange={(e) => setRegisterinfo({...registerInfo, password: e.target.value})}
+            value = {registerInfo.password}
+            required
+            />
+
+            <br />
+
+            <label htmlFor="name3"> Name: </label>
+            <input
+            id="name3"
+            type="text"
+            onChange={(e) => setRegisterinfo({...registerInfo, name: e.target.value})}
+            value = {registerInfo.name}
+            required
+            />
+
+            <br />
+
+            <label htmlFor="location4"> Location: </label>
+            <input
+            id="location4"
+            type="text"
+            onChange={(e) => setRegisterinfo({...registerInfo, location: e.target.value})}
+            value = {registerInfo.location}
+            required
+            />
+
+            <br />
+
+            {error ? <small className="error">{error}</small> : null}
+
+            <button className="submitButton" type="submit">
+            SUBMIT
+            </button>
+
+        </form>
+        
+        </Popup>
+
     </div>
   );
 };
