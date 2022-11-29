8f4a914ef3da8820ec87d8e188b0e425b103c376
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-11-29 13:30:27 -0600

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 
 const Home = () => {
+
+    const homeImage = [
+        "https://media.istockphoto.com/id/1254508881/photo/woman-holding-sale-shopping-bags-consumerism-shopping-lifestyle-concept.jpg?s=612x612&w=0&k=20&c=wuS3z6nPQkMM3_wIoO67qQXP-hfXkxlBc2sedwh-hxc=",
+    ]
+
+    const holidayImages = [
+        "https://live.staticflickr.com/65535/50656940087_a16e643316_b.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgw7HiIjlQPS1o2a_7-82UbBt7NwQcGhKoTQ&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKC6K3O6mURri30fKS3LeLYBxnK2s1tSmZAQ&usqp=CAU",
+    ]
+
     return (
         <div id="Home">
-            This is home
+            <div id="homeHeader"> Shop for any occasion and for any environment with Amazon Lite (name still pending) </div>
+            <div id="homeImage">
+                <img
+                src={homeImage}
+                alt="images"
+                />
+                </div>
+            <div id="row"> Shop for the holidays! </div>
+            <div id="holidayImages"> {holidayImages.map((image, i) => (
+                <img
+                id="holidayImages"
+                key={i} 
+                src={image}
+                alt="images"
+                />))}
+                </div>
         </div>
+
     )
 
 }
