e9f5a5bd12eb2921400a3aaf4a0ac3e5e19e8317
timothy <timothybrennan8@gmail.com>
2022-11-29 21:19:39 -0500

    did some stuff with the footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,39 @@
 import React from "react";
-import { HomeFooter } from "./"
+import { HomeFooter } from "./";
 
 const Home = () => {
+  const homeImage = [
+    "https://media.istockphoto.com/id/1254508881/photo/woman-holding-sale-shopping-bags-consumerism-shopping-lifestyle-concept.jpg?s=612x612&w=0&k=20&c=wuS3z6nPQkMM3_wIoO67qQXP-hfXkxlBc2sedwh-hxc=",
+  ];
 
-    const homeImage = [
-        "https://media.istockphoto.com/id/1254508881/photo/woman-holding-sale-shopping-bags-consumerism-shopping-lifestyle-concept.jpg?s=612x612&w=0&k=20&c=wuS3z6nPQkMM3_wIoO67qQXP-hfXkxlBc2sedwh-hxc=",
-    ]
+  const holidayImages = [
+    "https://live.staticflickr.com/65535/50656940087_a16e643316_b.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgw7HiIjlQPS1o2a_7-82UbBt7NwQcGhKoTQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKC6K3O6mURri30fKS3LeLYBxnK2s1tSmZAQ&usqp=CAU",
+  ];
 
-    const holidayImages = [
-        "https://live.staticflickr.com/65535/50656940087_a16e643316_b.jpg",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgw7HiIjlQPS1o2a_7-82UbBt7NwQcGhKoTQ&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKC6K3O6mURri30fKS3LeLYBxnK2s1tSmZAQ&usqp=CAU",
-    ]
-
-    return (
-
-        <div id="Home">
-
-            <div id="homeHeader"> Shop for any occasion with Amazon Lite (name still pending) </div>
-            <div id="homeImage">
-                <img
-                src={homeImage}
-                alt="images"
-                />
-                </div>
-            <div id="row"> 
-            <div className="rowText">
-                Buy presents for the holidays!
-                </div>
-            <div id="holidayImages"> {holidayImages.map((image, i) => (
-                <img
-                id="holidayImages"
-                key={i} 
-                src={image}
-                alt="images"
-                />))}
-                </div>
-                </div>
-                
-                <HomeFooter />
-                
+  return (
+    <div id="Home">
+      <div id="homeHeader">
+        {" "}
+        Shop for any occasion with Amazon Lite (name still pending){" "}
+      </div>
+      <div id="homeImage">
+        <img src={homeImage} alt="images" />
+      </div>
+      <div id="row">
+        <div className="rowText">Buy presents for the holidays!</div>
+        <div id="holidayImages">
+          {" "}
+          {holidayImages.map((image, i) => (
+            <img id="holidayImages" key={i} src={image} alt="images" />
+          ))}
         </div>
+      </div>
 
-    )
+      <HomeFooter />
+    </div>
+  );
+};
 
-}
-
-export default Home
+export default Home;
--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 
 const HomeFooter = () => {
+  return (
+    <div className="homeFooterContainer">
+      <div>
+        <div className="footerColumns">
+          <ul className="footerRows">
+            <h4>Contact Us</h4>
+            <li> Charles </li>
+            <li> Tim </li>
+            <li> Minh </li>
+            <li> Andrew </li>
+          </ul>
 
-    return (
-        <div className="homeFooterContainer">
-            <div>
-            <div className="footerColumns">
-            <ul className="footerRows"> Contact Us
-                <li > Charles </li>
-                <li> Tim </li>
-                <li> Minh </li>
-                <li> Andrew </li>
-            </ul>
-            
-            <ul className="footerRows"> Donate to us
-                <li> Broke boy funds </li>
-            </ul>
-            <ul className="footerRows"> Come see our Portfolios!
-                <li> Charles </li>
-                <li> Tim </li>
-                <li> Minh </li>
-                <li> Andrew </li>
-            </ul>
+          <ul className="footerRows">
+            <h4>Donate to us</h4>
+            <li> Broke boy funds </li>
+          </ul>
+          <ul className="footerRows">
+            <h4> Come see our Portfolios!</h4>
+            <li> Charles </li>
+            <li> Tim </li>
+            <li> Minh </li>
+            <li> Andrew </li>
+          </ul>
         </div>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default HomeFooter 
+export default HomeFooter;
