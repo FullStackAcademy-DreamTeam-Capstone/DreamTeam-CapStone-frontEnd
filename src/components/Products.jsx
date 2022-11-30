75e36c311970db3b97587e201a3789295e4aea5b
minimyl <aandrew.myles@gmail.com>
2022-11-30 13:43:33 -0500

    profile update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,26 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Products = (props) => {
   const products = props.getProduct;
 
-
   return (
-    <div id="productList">
-      {products.length ? (
-        products.map((product) => {
-          return (
-            <div id="products" key={`product-${product.id}`}>
-              <div id="singleProduct">
-                <div>{product.name}</div>
-                <div>{product.price}</div>
-                <div>{product.img_url}</div>
+    <div>
+      <div id="productList">
+        {products.length ? (
+          products.map((product) => {
+            return (
+              <div id="products" key={`product-${product.id}`}>
+                <div id="singleProduct">
+                  <div>{product.name}</div>
+                  <div>{product.price}</div>
+                  <div>{product.img_url}</div>
+                </div>
               </div>
-            </div>
-          );
-        })
-      ) : (
-        <div>Loading Products..</div>
-      )}
+            );
+          })
+        ) : (
+          <div>Loading Products..</div>
+        )}
+      </div>
+      <NavLink to="/products/create"><button>Create Products</button></NavLink>
     </div>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,40 +1,42 @@
-import React, {useEffect} from "react";
-import {userInfo } from "../apiAdapter";
+import React, { useEffect } from "react";
+import { userInfo } from "../apiAdapter";
 
 const Profile = (props) => {
-  console.log(props, "PROPS");
+ 
   const loggedIn = props.loggedIn;
-  const setCurrentUser = props.setCurrentUser
-  const currentUser = props.currentUser
-console.log(currentUser, "THIS IS CURRENTUSER")
+  const setCurrentUser = props.setCurrentUser;
+  const currentUser = props.currentUser;
+
   useEffect(() => {
-      async function fetchData() {
-        const data = await userInfo();
-        console.log(data, "THIS IS DATA")
-        setCurrentUser(data);
-      };
-      fetchData();
-    }, [loggedIn]);
+    async function fetchData() {
+      const data = await userInfo();
+   
+      setCurrentUser(data);
+    }
+    fetchData();
+  }, [loggedIn]);
 
   return (
     <div>
-      {currentUser.name ? (
-        <>
-              <div>{currentUser.name}</div>
-              <div>{currentUser.username}</div>
-              <div>{currentUser.location}</div>
-        </>        
-      ) : (
-        <>
-          {loggedIn ? (
-            <div>
-              <div>Your Profile</div>
-            </div>
-          ) : (
-            <div>Please Log In</div>
-          )}
-        </>
-      )}
+      <div id="profiles">
+        {currentUser.name ? (
+          <>
+            <div>{currentUser.name}</div>
+            <div>{currentUser.username}</div>
+            <div>{currentUser.location}</div>
+          </>
+        ) : (
+          <>
+            {loggedIn ? (
+              <div>
+                <div>Your Profile</div>
+              </div>
+            ) : (
+              <div>Please Log In</div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
