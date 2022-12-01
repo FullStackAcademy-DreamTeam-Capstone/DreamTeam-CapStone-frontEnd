e207a84a958136aa5cf2540a68f5ccc2ebd7f883
Minh Nguyen <lambandwoof@gmail.com>
2022-12-01 14:27:37 -0600

    added cart in index and jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,22 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import { NavLink } from "react-router-dom"
+import { getCart } from "../apiAdapter";
+import Products from "./Products";
+import Profile from "./Profile";
 
-const Cart = () => {
+
+const Cart = (props) => {
+    const cart = props.getCart;
+
 
     //async delete cart item function
-
+    
     //async edit cart item function
- 
+
     //function to add all the item prices to get total price
 
     //buy button, does nothing but clear the cart and say that you have purchased the items
 
 
 return (
-    <div className="cartContainer">
-        <div> I am cart </div>
+    <div>
+      <div id="cartList">
+        {cart.length ? (
+          cart.map((cart) => {
+            return (
+              <div id="carts" key={`cart-${cart.id}`}>
+                <div id="singleCart">
+                  <div>{cart.user_id}</div>
+                  <div>$ {cart.isActive}</div>
+                </div>
+
+              </div>
+            );
+          })
+        ) : (
+          <div>Loading Cart..</div>
+        )}
+      </div>
     </div>
-)
+  );
 }
 
-export default Cart
+export default Cart;
