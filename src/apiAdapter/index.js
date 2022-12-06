5763997d62b9d73310298f497f06b8e6de6fdb0c
minimyl <aandrew.myles@gmail.com>
2022-12-06 15:37:38 -0500

    pushing for deploy

--- src/apiAdapter/index.js
+++ src/apiAdapter/index.js
@@ -1,6 +1,7 @@
 
 
-const BASE_URL = "http://localhost:8080";
+//const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://dreamteam-server.onrender.com"
 
 
 export async function getAllUsers() {
